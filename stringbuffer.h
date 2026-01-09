fae742dbe4871b013662221fac8f43eea6f7757a
hdhaussy <hdhaussy@gmail.com>
2026-01-09 15:33:07 +0100

    reworked

--- stringbuffer.h
+++ stringbuffer.h
@@ -36,10 +36,8 @@ struct stringbuffer {
 
 #ifdef __cplusplus
 	#define SB(str) { 0, sizeof(str)-1, (char*) (str) }
-	#define SB_EMPTY { 0, 0, (char*) "" }
 #else
 	#define SB(str) ((stringbuffer_t) { 0, sizeof(str)-1, (char*) (str) })
-	#define SB_EMPTY ((stringbuffer_t) { 0, 0, (char*) "" })
 #endif
 
 /**
@@ -49,24 +47,111 @@ struct stringbuffer {
  * The string is truncated to the desired size is less than the current length.
  */
 void sb_alloc(stringbuffer_t* sb,size_t desired);
+
+/**
+ * Frees the memory allocated for the stringbuffer's internal buffer
+ * @param sb a pointer to the stringbuffer
+ */
 void sb_release(stringbuffer_t* sb);
+
+/**
+ * Copies the content of a source stringbuffer to a destination stringbuffer
+ * @param dest the destination stringbuffer
+ * @param src the source stringbuffer
+ */
 void sb_copy(stringbuffer_t* dest,const stringbuffer_t* src);
+
+/**
+ * Returns a null-terminated C string representing the content of the stringbuffer
+ * @param sb a pointer to the stringbuffer
+ * @return the C string
+ */
 const char* sb_str(stringbuffer_t* sb);
 
+/**
+ * Appends the content of a source stringbuffer to the end of the destination stringbuffer
+ * @param dest the destination stringbuffer
+ * @param src the source stringbuffer
+ */
 void sb_append(stringbuffer_t* dest,const stringbuffer_t* src);
+
+/**
+ * Appends a C string to the end of the stringbuffer
+ * @param dest the destination stringbuffer
+ * @param src the C string to append
+ */
 void sb_append_str(stringbuffer_t* dest,const char* src);
+
+/**
+ * Appends a character to the end of the stringbuffer
+ * @param dest the destination stringbuffer
+ * @param c the character to append
+ */
 void sb_append_char(stringbuffer_t* dest,const char c);
+
+/**
+ * Inserts the content of a source stringbuffer at a given position in the destination stringbuffer
+ * @param dest the destination stringbuffer
+ * @param pos the insertion position
+ * @param src the source stringbuffer
+ */
 void sb_insert(stringbuffer_t* dest,size_t pos,const stringbuffer_t* src);
+
+/**
+ * Inserts a character at a given position in the stringbuffer
+ * @param dest the destination stringbuffer
+ * @param pos the insertion position
+ * @param c the character to insert
+ */
 void sb_insert_char(stringbuffer_t* dest,size_t pos,const char c);
+
+/**
+ * Replaces a portion of the stringbuffer with the content of another stringbuffer
+ * @param dest the destination stringbuffer
+ * @param pos the starting position
+ * @param len the length to replace
+ * @param src the source stringbuffer
+ */
 void sb_replace(stringbuffer_t* dest,size_t pos,size_t len,const stringbuffer_t* src);
+
+/**
+ * Appends a formatted string to the stringbuffer using printf-style formatting
+ * @param sb the stringbuffer
+ * @param fmt the format string
+ * @param ... the variable arguments
+ */
 void sb_printf(stringbuffer_t* sb,const char* fmt,...);
+
+/**
+ * Compares two stringbuffers
+ * @param sb1 the first stringbuffer
+ * @param sb2 the second stringbuffer
+ * @return a negative integer if sb1 < sb2, 0 if equal, positive if sb1 > sb2
+ */
 int sb_cmp(stringbuffer_t* sb1,stringbuffer_t* sb2);
+
+/**
+ * Converts all characters in the stringbuffer to uppercase
+ * @param sb the stringbuffer
+ */
 void sb_upper(stringbuffer_t* sb);
+
+/**
+ * Converts all characters in the stringbuffer to lowercase
+ * @param sb the stringbuffer
+ */
 void sb_lower(stringbuffer_t* sb);
 
+/** Returns a pointer to the stringbuffer's internal buffer */
 static inline char* sb_buffer(stringbuffer_t* sb) { return sb->buffer; }
+
+/** Returns the current length of the stringbuffer's content */
 static inline size_t sb_length(stringbuffer_t* sb) { return sb->length; }
+
+/** Returns the current capacity of the stringbuffer's buffer */
 static inline size_t sb_capacity(stringbuffer_t* sb) { return sb->capacity; }
+
+/** Ensures the buffer capacity is at least the desired size, reallocating if necessary */
 static inline char sb_ensure_capacity(stringbuffer_t* sb,size_t desired) {
 	size_t alloc = sb->capacity;
 	if(alloc < desired) {
@@ -76,6 +161,25 @@ static inline char sb_ensure_capacity(stringbuffer_t* sb,size_t desired) {
 	}
 	return sb->capacity >= desired;
 }
+
+/** Returns the available space in the buffer (capacity - length) */
 static inline size_t sb_available(stringbuffer_t* sb) { return sb->capacity>0?sb->capacity-sb->length:0; }
+
+/**
+ * Extract a substring from the stringbuffer
+ * @param sb the source stringbuffer
+ * @param start the starting index
+ * @param len the length of the substring
+ * @return a new stringbuffer containing the substring
+ */
+stringbuffer_t sb_substr(stringbuffer_t* sb, size_t start, size_t len);
+
+/**
+ * Find the first occurrence of a substring in the stringbuffer
+ * @param sb the stringbuffer to search in
+ * @param substr the stringbuffer containing the substring to find
+ * @return the index of the first occurrence, or -1 if not found
+ */
+size_t sb_find(stringbuffer_t* sb, stringbuffer_t* substr);
 
 #endif // #ifndef __include_stringbuffer_h
--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #include "stringbuffer.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 static int nbtests = 0;
 static int nbfails = 0;
@@ -25,20 +26,17 @@ void test_sb_str() {
 	ASSERT(sb_capacity(&sb)==0);
 	ASSERT(strcmp(sb_str(&sb),"Hello")==0);
 	ASSERT(sb_capacity(&sb)==0);
-	sb_release(&sb);
 	sb = SB("");
 	ASSERT(strcmp(sb_str(&sb),"")==0);
-	sb_release(&sb);
-	sb = SB_EMPTY;
+	sb = SB("");
 	ASSERT(strcmp(sb_str(&sb),"")==0);
-	sb_release(&sb);
 }
 
 void test_sb_append_str() {
 	stringbuffer_t sb = SB("Hello");
 	sb_append_str(&sb,", World");
 	sb_append_str(&sb," !");
-	ASSERT(strcmp(sb_str(&sb),"Hello, World !")==0);
+	ASSERT(sb_cmp(&sb, &SB("Hello, World !"))==0);
 	sb_release(&sb);
 }
 
@@ -47,14 +45,14 @@ void test_sb_append_char() {
 	sb_append_char(&sb,'r');
 	sb_append_char(&sb,'l');
 	sb_append_char(&sb,'d');
-	ASSERT(strcmp(sb_str(&sb),"World")==0);
+	ASSERT(sb_cmp(&sb, &SB("World"))==0);
 	sb_release(&sb);
 }
 
 void test_sb_printf() {
 	stringbuffer_t sb = SB("The response");
 	sb_printf(&sb," %s %d !","is",42);
-	ASSERT(strcmp(sb_str(&sb),"The response is 42 !")==0);
+	ASSERT(sb_cmp(&sb, &SB("The response is 42 !"))==0);
 	sb_release(&sb);
 }
 
@@ -62,7 +60,7 @@ void test_sb_insert() {
 	stringbuffer_t sb = SB("Roll");
 	sb_insert(&sb,0,&SB("Rock"));
 	sb_insert(&sb,4,&SB("'n'"));
-	ASSERT(strcmp(sb_str(&sb),"Rock'n'Roll")==0);
+	ASSERT(sb_cmp(&sb, &SB("Rock'n'Roll"))==0);
 	sb_release(&sb);
 }
 
@@ -70,21 +68,21 @@ void test_sb_insert_char() {
 	stringbuffer_t sb = SB("Hell here !");
 	sb_insert_char(&sb,4,'o');
 	sb_insert_char(&sb,6,'t');
-	ASSERT(strcmp(sb_str(&sb),"Hello there !")==0);
+	ASSERT(sb_cmp(&sb, &SB("Hello there !"))==0);
 	sb_release(&sb);
 }
 
 void test_sb_replace() {
 	stringbuffer_t sb = SB("Hello, toto!");
 	sb_replace(&sb,7,4,&SB("World "));
-	ASSERT(strcmp(sb_str(&sb),"Hello, World !")==0);
+	ASSERT(sb_cmp(&sb, &SB("Hello, World !"))==0);
 	sb_release(&sb);
 }
 
 void test_sb_cmp() {
-	ASSERT(sb_cmp(&SB_EMPTY,&SB(""))==0);
-	ASSERT(sb_cmp(&SB_EMPTY,&SB("ABC"))==-1);
-	ASSERT(sb_cmp(&SB("ABC"),&SB_EMPTY)==1);
+	ASSERT(sb_cmp(&SB(""),&SB(""))==0);
+	ASSERT(sb_cmp(&SB(""),&SB("ABC"))==-1);
+	ASSERT(sb_cmp(&SB("ABC"),&SB(""))==1);
 	ASSERT(sb_cmp(&SB("ABC"),&SB("ABC"))==0);
 	ASSERT(sb_cmp(&SB("ABC"),&SB("DEF"))==-1);
 	ASSERT(sb_cmp(&SB("DEF"),&SB("ABC"))==1);
@@ -95,15 +93,105 @@ void test_sb_cmp() {
 void test_sb_upper() {
 	stringbuffer_t sb = SB("speak loud !");
 	sb_upper(&sb);
-	ASSERT(strcmp(sb_str(&sb),"SPEAK LOUD !")==0);
+	ASSERT(sb_cmp(&sb, &SB("SPEAK LOUD !"))==0);
 	sb_release(&sb);
 }
 
 void test_sb_lower() {
 	stringbuffer_t sb = SB("BE QUIET !");
 	sb_lower(&sb);
-	ASSERT(strcmp(sb_str(&sb),"be quiet !")==0);
+	ASSERT(sb_cmp(&sb, &SB("be quiet !"))==0);
 	sb_release(&sb);
+}
+
+void test_sb_alloc() {
+	stringbuffer_t sb = SB("");
+	sb_alloc(&sb, 10);
+	ASSERT(sb_capacity(&sb) >= 10);
+	sb_release(&sb);
+}
+
+void test_sb_release() {
+	stringbuffer_t sb = SB("");
+	sb_append_str(&sb, "test");
+	sb_release(&sb);
+	ASSERT(sb_capacity(&sb) == 0);
+	ASSERT(sb_length(&sb) == 0);
+}
+
+void test_sb_copy() {
+	stringbuffer_t src = SB("");
+	sb_append_str(&src, "Hello World");
+	stringbuffer_t dest = SB("");
+	sb_copy(&dest, &src);
+	ASSERT(sb_cmp(&dest, &SB("Hello World")) == 0);
+	sb_release(&src);
+	sb_release(&dest);
+}
+
+void test_sb_append() {
+	stringbuffer_t sb1 = SB("");
+	sb_append_str(&sb1, "Hello");
+	stringbuffer_t sb2 = SB("");
+	sb_append_str(&sb2, " World");
+	sb_append(&sb1, &sb2);
+	ASSERT(sb_cmp(&sb1, &SB("Hello World")) == 0);
+	sb_release(&sb1);
+	sb_release(&sb2);
+}
+
+void test_sb_buffer() {
+	stringbuffer_t sb = SB("");
+	sb_append_str(&sb, "test");
+	ASSERT(sb_buffer(&sb) == sb.buffer);
+	sb_release(&sb);
+}
+
+void test_sb_length() {
+	stringbuffer_t sb = SB("hello");
+	ASSERT(sb_length(&sb) == 5);
+}
+
+void test_sb_capacity() {
+	stringbuffer_t sb = SB("");
+	sb_alloc(&sb, 20);
+	ASSERT(sb_capacity(&sb) >= 20);
+	sb_release(&sb);
+}
+
+void test_sb_ensure_capacity() {
+	stringbuffer_t sb = SB("");
+	sb_ensure_capacity(&sb, 15);
+	ASSERT(sb_capacity(&sb) >= 15);
+	sb_release(&sb);
+}
+
+void test_sb_available() {
+	stringbuffer_t sb = SB("");
+	sb_append_str(&sb, "hi");
+	size_t avail = sb_available(&sb);
+	ASSERT(avail == 2);
+	sb_release(&sb);
+}
+
+void test_sb_substr() {
+	stringbuffer_t sb = SB("Hello World");
+	stringbuffer_t substr = sb_substr(&sb, 6, 5);
+	ASSERT(sb_cmp(&substr, &SB("World")) == 0);
+	sb_release(&substr);
+}
+
+void test_sb_find() {
+	stringbuffer_t sb = SB("Hello World");
+	stringbuffer_t substr = SB("World");
+	size_t pos = sb_find(&sb, &substr);
+	ASSERT(pos == 6);
+	substr = SB("NotFound");
+	pos = sb_find(&sb, &substr);
+	ASSERT(pos == -1);
+	substr = SB("");
+	pos = sb_find(&sb, &substr);
+	ASSERT(pos == 0);
 }
 
 int main(int argc,char** argv) {
@@ -117,6 +205,17 @@ int main(int argc,char** argv) {
 	RUN_TEST(test_sb_cmp);
 	RUN_TEST(test_sb_upper);
 	RUN_TEST(test_sb_lower);
+	RUN_TEST(test_sb_alloc);
+	RUN_TEST(test_sb_release);
+	RUN_TEST(test_sb_copy);
+	RUN_TEST(test_sb_append);
+	RUN_TEST(test_sb_buffer);
+	RUN_TEST(test_sb_length);
+	RUN_TEST(test_sb_capacity);
+	RUN_TEST(test_sb_ensure_capacity);
+	RUN_TEST(test_sb_available);
+	RUN_TEST(test_sb_substr);
+	RUN_TEST(test_sb_find);
 	printf("%d tests passed, %d tests failed.\n",nbtests,nbfails);
 	return nbfails;
 }
